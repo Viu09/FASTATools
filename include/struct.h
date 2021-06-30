ef6e598303ac1dd2b0b4e8c4648ef100af39d295
Silviu Zaino <silviu.zaino@epitech.eu>
2021-06-30 10:12:13 +0200

    {Init} Begin of project

---
+++ include/struct.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** FASTA [WSL: Ubuntu-20.04]
+** File description:
+** struct
+*/
+
+#ifndef STRUCT_H_
+#define STRUCT_H_
+
+
+
+#endif /* !STRUCT_H_ */
---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** FASTA [WSL: Ubuntu-20.04]
+** File description:
+** main
+*/
+
+#include "include/struct.h"
+
+int main(int ac, char **av)
+{
+    return (0);
+}
